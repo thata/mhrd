a41c5369c5a9a707a4aa917588d2c17f5087ed1d
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2019-10-20 12:51:35 +0900

    Add NAND, NOT and AND.

---
+++ nand/nand_test.v
@@ -0,0 +1,20 @@
+module nand_test;
+    reg in1, in2;
+    wire out;
+
+    NAND dut(in1, in2, out);
+
+    initial begin
+        in1 = 0; in2 = 0; #10;
+        assert(out == 1) $display("PASSED"); else $display("FAILED");
+
+        in1 = 1; in2 = 0; #10;
+        assert(out == 1) $display("PASSED"); else $display("FAILED");
+
+        in1 = 0; in2 = 1; #10;
+        assert(out == 1) $display("PASSED"); else $display("FAILED");
+
+        in1 = 1; in2 = 1; #10;
+        assert(out == 0) $display("PASSED"); else $display("FAILED");
+    end
+endmodule
--- not/not_test.v
+++ not/not_test.v
@@ -1,5 +1,14 @@
 module not_test();
+    reg in;
+    wire out;
+
+    NOT dut(in, out);
+
     initial begin
-        $display("test");
+        in = 0; #10;
+        assert(out == 1) $display("PASSED"); else $display("FAILED");
+
+        in = 1; #10;
+        assert(out == 0) $display("PASSED"); else $display("FAILED");
     end
 endmodule
