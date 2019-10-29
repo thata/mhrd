bddcf0f55e30c94bfad4b0028cdbc24a23bddbf0
Takashi Hatakeyama <t.hatakeyama@MacBook-Pro-4.local>
2019-10-29 14:10:25 +0900

    Add MUX4B

--- mux/mux4b.v
+++ mux/mux4b.v
@@ -1,14 +1,13 @@
 module MUX4B(
     input sel, 
     input [3:0] in1, in2,
-    output out
+    output logic[3:0] out
 );
 
-    wire enable_in1;
-    wire and1_out, and2_out;
-
-    NOT not1(sel, enable_in1);
-    AND and1(in1, enable_in1, and1_out);
-    AND and2(in2, sel, and2_out);
-    OR or1(and1_out, and2_out, out);
+    always_comb begin
+        case (sel)
+            0: out = in1;
+            1: out = in2;
+        endcase
+    end
 endmodule
--- mux/mux4b_test.v
+++ mux/mux4b_test.v
@@ -1,39 +1,34 @@
 // MUX4B testbench
-// iverilog -g 2012 -s mux_test *.v && ./a.out 
+// iverilog -g 2012 -s mux4b_test *.v && ./a.out
 module mux4b_test;
-    reg sel, in1, in2;
-    wire out;
+    logic sel;
+    logic [3:0] in1, in2, out;
 
-    MUX dut(sel, in1, in2, out);
+    MUX4B dut(sel, in1, in2, out);
 
     initial begin
-        // sel: 0, in1: 0, in2: 0 => out: 0
-        sel = 0; in1 = 0; in2 = 0; #10;
-        assert(out == 0) $display("PASSED"); else $display("FAILED (%b)", out);
+        // sel: 0, in1: 0000, in2: 0000 => out: 0000
+        sel = 0; in1 = 4'b0000; in2 = 4'b0000; #10;
+        assert(out == 4'b0000) $display("PASSED"); else $display("FAILED (%b)", out);
 
-        // sel: 0, in1: 1, in2: 0 => out: 1
-        sel = 0; in1 = 1; in2 = 0; #10;
-        assert(out == 1) $display("PASSED"); else $display("FAILED (%b)", out);
+        // sel: 0, in1: 0001, in2: 0010 => out: 0001
+        sel = 0; in1 = 4'b0001; in2 = 4'b0010; #10;
+        assert(out == 4'b0001) $display("PASSED"); else $display("FAILED (%b)", out);
 
+        // sel: 0, in1: 1111, in2: 0010 => out: 1111
+        sel = 0; in1 = 4'b1111; in2 = 4'b0010; #10;
+        assert(out == 4'b1111) $display("PASSED"); else $display("FAILED (%b)", out);
 
-        // sel: 0, in1: 1, in2: 1 => out: 1
-        sel = 0; in1 = 1; in2 = 1; #10;
-        assert(out == 1) $display("PASSED"); else $display("FAILED (%b)", out);
+        // sel: 1, in1: 0000, in2: 0000 => out: 0000
+        sel = 1; in1 = 4'b0000; in2 = 4'b0000; #10;
+        assert(out == 4'b0000) $display("PASSED"); else $display("FAILED (%b)", out);
 
-        // sel: 1, in1: 0, in2: 0 => out: 0
-        sel = 1; in1 = 0; in2 = 0; #10;
-        assert(out == 0) $display("PASSED"); else $display("FAILED (%b)", out);
+        // sel: 1, in1: 0000, in2: 0001 => out: 0001
+        sel = 1; in1 = 4'b0000; in2 = 4'b0001; #10;
+        assert(out == 4'b0001) $display("PASSED"); else $display("FAILED (%b)", out);
 
-        // sel: 1, in1: 1, in2: 0 => out: 0
-        sel = 1; in1 = 1; in2 = 0; #10;
-        assert(out == 0) $display("PASSED"); else $display("FAILED (%b)", out);
-
-        // sel: 1, in1: 0, in2: 1 => out: 1
-        sel = 1; in1 = 0; in2 = 1; #10;
-        assert(out == 1) $display("PASSED"); else $display("FAILED (%b)", out);
-
-        // sel: 1, in1: 1, in2: 1 => out: 1
-        sel = 1; in1 = 1; in2 = 1; #10;
-        assert(out == 1) $display("PASSED"); else $display("FAILED (%b)", out);
+        // sel: 1, in1: 0000, in2: 1111 => out: 1111
+        sel = 1; in1 = 4'b0010; in2 = 4'b1111; #10;
+        assert(out == 4'b1111) $display("PASSED"); else $display("FAILED (%b)", out);
     end
 endmodule
